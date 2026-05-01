bf97e9deccb068968ce3d4ab3588789999b51a97
Brajesh Das <brajeshdas117@gmail.com>
2026-05-01 14:33:45 +0000

    Target Sum

---
+++ Target_sum.py
@@ -0,0 +1,20 @@
+def two_sum (numbers,target):
+
+    left = 0
+    right = len(numbers) - 1
+
+    while (left < right):
+        total = numbers[left] + numbers[right]
+
+        # handle all corner cases :
+
+        if (total == target):
+            return [left + 1,right + 1]
+        
+        elif (total < target):
+            left += 1
+        
+        else:
+            right -= 1
+    
+    return []
